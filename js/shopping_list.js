7a1255da32e7927b6bb5aea8f26fb0063cf98c97
lalit137 <lalit.saud137@gmail.com>
2023-05-07 14:57:17 +0545

    optimize a code

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -2,21 +2,47 @@ let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {
-        let li = document.createElement("li");
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    let li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
         input.value = "";
+}
+
+button.addEventListener("click", function() {
+    if (inputLength() > 0) {
+        createListElement();
     }
 })
 
 input.addEventListener("keypress", function(event) {
-    if(input.value.length > 0 && event.keyCode === 13) {
-        let li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = ""
+    if(inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
     }
     
 })
+
+
+
+
+// button.addEventListener("click", function() {
+//     if (input.value.length > 0) {
+//         let li = document.createElement("li");
+//         li.appendChild(document.createTextNode(input.value));
+//         ul.appendChild(li);
+//         input.value = "";
+//     }
+// })
+
+// input.addEventListener("keypress", function(event) {
+//     if(input.value.length > 0 && event.keyCode === 13) {
+//         let li = document.createElement("li");
+//         li.appendChild(document.createTextNode(input.value));
+//         ul.appendChild(li);
+//         input.value = ""
+//     }
+// })
